b67f0064b6b37bbd24c2a51e1e6cc319a266d66a
marchisio <sdusunlin@gmail.com>
2014-11-16 15:25:32 -0700

    clean the folder

--- NeuralNetworksProjectFolder/FlatWorldIIV1.0ClassVersion_dist2014/dat/arch2_plot.m
+++ NeuralNetworksProjectFolder/FlatWorldIIV1.0ClassVersion_dist2014/dat/arch2_plot.m
@@ -12,16 +12,11 @@ b = C{3};
 b = b(2:end, :);
 Y = transpose(reshape(C{3}, [], 20));
 
-<<<<<<< HEAD
-errorbar(C{1}, C{3}, E);hold on
-title('');
-=======
 M = mean(Y, 2);
 E = std(Y, 1, 2);
 
 
 errorbar(X, M, E);
 title('Architecture 2 Life Time vs Speed');
->>>>>>> FETCH_HEAD
 xlabel('speed');
 ylabel('simulation life');
